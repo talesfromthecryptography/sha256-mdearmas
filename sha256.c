33261dd94ec232aa742d3a21766b54647e3e68ce
Makenzie De Armas <dearm102@mail.chapman.edu>
2019-10-01 20:56:36 +0000

    improved efficiency of sha256_transform

--- sha256.c
+++ sha256.c
@@ -58,44 +58,30 @@ static const uint32_t init_digest[SHA256_DIGEST_SIZE] = {
 
 void sha256_transform(sha256_state *state)
 {
-
-  // Improve the efficiency of this code.
-  //  1. Reduce memory usage by re-cycling w values
-  //  2. Find a way to reduce copying (lines 83-90)
-  //
-  //  Consider re-ordering some code
-  //
-	uint32_t a, b, c, d, e, f, g, h, t1, t2, w[NUM_ROUNDS];
-  uint8_t  i;
+	uint32_t a, b, c, d, e, f, g, h, t1, t2, placeholder, w[16], temp[8];
+  uint8_t  i, j;
 
 	for (i = 0; i < 16; ++i)
 		w[i] = state->buffer[i];
-	for ( ; i < 64; ++i)
-		w[i] = SIG1(w[i - 2]) + w[i - 7] + SIG0(w[i - 15]) + w[i - 16];
 
-	a = state->digest[0]; //temp[base]
-	b = state->digest[1]; //temp[base+1]
-	c = state->digest[2]; //temp[base+2]
-	d = state->digest[3];
-	e = state->digest[4];
-	f = state->digest[5];
-	g = state->digest[6];
-	h = state->digest[7];
+	for(j = 0; j < 8; j++)
+		temp[i] = state->digest[i];
+
+	uint8_t base = 0; //marks the start of the temp array
+	uint8_t end = 0; //marks current end of w[]
 
 	for (i = 0; i < 64; ++i) {
-		t1 = h + EP1(e) + CH(e,f,g) + k[i] + w[i];
-		t2 = EP0(a) + MAJ(a,b,c);
-		h = g;
-		g = f;
-		f = e;
-		e = d + t1;
-		d = c;
-		c = b;
-		b = a;
-		a = t1 + t2;
-		//base = (base-1) % 8;
+		t1 = temp[(base + 7) % 8] + EP1(temp[(base + 4) % 8]) + CH(temp[(base + 4) % 8],temp[(base + 5) % 8],temp[(base + 6) % 8]) + k[i] + w[i % 16];
+		t2 = EP0(temp[base]) + MAJ(temp[base],temp[(base + 1) % 8],temp[(base + 2) % 8]);
+		temp[(base + 4) % 8] = temp[(base + 3) % 8] + t1;
+		temp[base] = t1 + t2;
+		base = (base-1) % 8;
+		if(i >= 16) {
+			placeholder = w[end];
+			w[end] = SIG1(w[(end-2)%16]) + w[(end-7)%16] + SIG0(w[(end-15)%16]) + placeholder;
+			end = (end+1) % 16;
+		}
 	}
-
 	state->digest[0] += a;
 	state->digest[1] += b;
 	state->digest[2] += c;
